84a09db8b676c2511a33a7c0d9aaad8131d84356
Constantin Gaul <Constantin.Gaul@gmail.com>
2015-01-04 17:27:13 +0100

    Throwing new NullPointerException, if dpidMap has no value for given dpid in Network.getSwitch(dpid).

--- src/main/java/net/onrc/openvirtex/elements/network/Network.java
+++ src/main/java/net/onrc/openvirtex/elements/network/Network.java
@@ -191,10 +191,10 @@ public abstract class Network<T1 extends Switch, T2 extends Port, T3 extends Lin
     public T1 getSwitch(final Long dpid) throws InvalidDPIDException {
         try {
             if(dpidMap.get(dpid) == null){
-                log.warn("getSwitch is null via dpidMap.get(dpid)!");
+                throw new NullPointerException("DPIDMap has no value for dpid "+ dpid);
             }
-
             return this.dpidMap.get(dpid);
+
         } catch (ClassCastException | NullPointerException ex) {
             log.warn("InvalidDPIDException should be thrown now...");
             throw new InvalidDPIDException("DPID "
